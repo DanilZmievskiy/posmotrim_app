d819f0ba724fb18cee3401ed823ece944ce1b99f
DanilZmievskiy <danil.zmievskiy@gmail.com>
2023-12-04 18:30:46 +0300

    Main Screen bottom navigation changes

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,6 +9,9 @@ import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:posmotrim_app/signin_page.dart';
 import 'package:posmotrim_app/movie.dart';
+import 'package:posmotrim_app/user_page.dart';
+import 'package:posmotrim_app/status_page.dart';
+import 'package:posmotrim_app/home_page_placeholder.dart';
 
 
 class MainPage extends StatefulWidget {
@@ -20,57 +23,20 @@ class _MainPageState extends State<MainPage>{
 
   late SharedPreferences sharedPreferences;
 
-  var movieData = [];
-  var horrorData = [];
-  var dramaData = [];
+  int _selectedIndex = 0;
+
+  final _widgetOptions = [
+    MainPagePlaceholder(),
+    StatusPage(),
+    UserPage()
+  ];
 
   @override
   void initState() {
     checkLoginStatus();
-    fetchMoviesByGenreData();
-    fetchHorrorData();
-    fetchDramaData();
     super.initState();
   }
 
-  Future<void> fetchMoviesByGenreData() async {
-    var requestBody = Uri.encodeFull("комедия/10");
-    final response = await http.get(Uri.parse('${dotenv.env['BACKEND_HTTP']}/films/top_films_by_genre/$requestBody'));
-    if (response.statusCode == 200) {
-      setState(() {
-        final decodedResponse = utf8.decode(response.bodyBytes);
-        movieData = json.decode(decodedResponse);
-      });
-    } else {
-      throw Exception('Не удалось получить данные о фильме');
-    }
-  }
-
-  Future<void> fetchHorrorData() async {
-    var requestBody = Uri.encodeFull("ужасы/10");
-    final response = await http.get(Uri.parse('${dotenv.env['BACKEND_HTTP']}/films/top_films_by_genre/$requestBody'));
-    if (response.statusCode == 200) {
-      setState(() {
-        final decodedResponse = utf8.decode(response.bodyBytes);
-        horrorData = json.decode(decodedResponse);
-      });
-    } else {
-      throw Exception('Не удалось получить данные о фильме');
-    }
-  }
-
-  Future<void> fetchDramaData() async {
-    var requestBody = Uri.encodeFull("драма/10");
-    final response = await http.get(Uri.parse('${dotenv.env['BACKEND_HTTP']}/films/top_films_by_genre/$requestBody'));
-    if (response.statusCode == 200) {
-      setState(() {
-        final decodedResponse = utf8.decode(response.bodyBytes);
-        dramaData = json.decode(decodedResponse);
-      });
-    } else {
-      throw Exception('Не удалось получить данные о фильме');
-    }
-  }
 
   checkLoginStatus() async {
     sharedPreferences = await SharedPreferences.getInstance();
@@ -95,95 +61,6 @@ class _MainPageState extends State<MainPage>{
               child: Text("Log Out", style: TextStyle(color: Colors.red))
           ),
         ],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            SizedBox(height: 20),
-            Text(
-              'Топ 10 в жанре комедия:',
-              style: TextStyle(
-                color: Colors.white,
-                fontFamily: 'Inter',
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 10),
-            SizedBox(
-              height: 300.0,
-              child: ListView.separated(
-                physics: ClampingScrollPhysics(),
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                padding: const EdgeInsets.all(12),
-                itemCount: movieData!.length,
-                separatorBuilder: (context, index) {
-                  return const SizedBox(width: 12);
-                },
-                itemBuilder: (context, index) {
-                  final movie = movieData![index];
-                  return movieCard(movie);
-                },
-              ),
-            ),
-            Text(
-              'Топ 10 в жанре ужасы:',
-              style: TextStyle(
-                color: Colors.white,
-                fontFamily: 'Inter',
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 10),
-            SizedBox(
-              height: 300.0,
-              child: ListView.separated(
-                physics: ClampingScrollPhysics(),
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                padding: const EdgeInsets.all(12),
-                itemCount: horrorData!.length,
-                separatorBuilder: (context, index) {
-                  return const SizedBox(width: 12);
-                },
-                itemBuilder: (context, index) {
-                  final movie = horrorData![index];
-                  return movieCard(movie);
-                },
-              ),
-            ),
-            Text(
-              'Топ 10 в жанре драма:',
-              style: TextStyle(
-                color: Colors.white,
-                fontFamily: 'Inter',
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 10),
-            SizedBox(
-              height: 300.0,
-              child: ListView.separated(
-                physics: ClampingScrollPhysics(),
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                padding: const EdgeInsets.all(12),
-                itemCount: dramaData!.length,
-                separatorBuilder: (context, index) {
-                  return const SizedBox(width: 12);
-                },
-                itemBuilder: (context, index) {
-                  final movie = dramaData![index];
-                  return movieCard(movie);
-                },
-              ),
-            )
-          ],
-        ),
       ),
       backgroundColor: Colors.grey.shade900,
       bottomNavigationBar: Container(
@@ -220,48 +97,19 @@ class _MainPageState extends State<MainPage>{
                 text: "User",
               )
             ],
+            selectedIndex: _selectedIndex,
+            onTabChange: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
           ),
         ),
       ),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _widgetOptions,
+      ),
     );
   }
-
-  Widget movieCard(movie) => GestureDetector(
-    child: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          child: Image.network(
-            'https://www.kinopoisk.ru/images/film_big/${movie['kinopoisk_id']}.jpg',
-            height: 200,
-            width: 200,
-            fit: BoxFit.cover,
-          ),
-        ),
-        const SizedBox(height: 8),
-        Text(
-          movie['name'].toString(),
-          style: TextStyle(
-            color: Colors.white,
-            fontFamily: 'Inter',
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-          ),
-          maxLines: 2, // Максимальное количество строк
-          overflow: TextOverflow.ellipsis, // Обрезание текста, если не помещается
-        ),
-      ],
-    ),
-    onTap: () {
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => MoviePage(
-            filmId: movie['kinopoisk_id'],
-          ),
-        ),
-      );
-    },
-  );
 }
